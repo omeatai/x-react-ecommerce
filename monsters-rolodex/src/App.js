7d12818092db5ee2234d98a3a64ad10403edcb60
omeatai <omeatai@gmail.com>
2023-10-13 19:04:02 -0600

    5. Using onClick Event with setState Function
    5. Using onClick Event with setState Function

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -5,8 +5,20 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      name: "Ifeanyi",
+      name: { firstName: "Ifeanyi", lastName: "Omeata" },
+      company: "STZ",
     };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange() {
+    this.setState({
+      name:
+        this.state.name.firstName === "Ifeanyi"
+          ? { firstName: "Mark", lastName: "Weber" }
+          : { firstName: "Ifeanyi", lastName: "Omeata" },
+    });
   }
 
   render() {
@@ -14,7 +26,11 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <h1>Hello World!</h1>
-          <h3>Hi, my name is {this.state.name}</h3>
+          <h3>
+            Hi, my name is {this.state.name.firstName}{" "}
+            {this.state.name.lastName}. I work at {this.state.company}.
+          </h3>
+          <button onClick={this.handleChange}>Change Name</button>
         </header>
       </div>
     );
