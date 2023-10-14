701b5748a6e77cd9007c5589dc09387e3432caf0
omeatai <omeatai@gmail.com>
2023-10-13 19:08:31 -0600

    6. Followup Callbacks within setState Functions
    6. Followup Callbacks within setState Functions

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -13,12 +13,17 @@ class App extends Component {
   }
 
   handleChange() {
-    this.setState({
-      name:
-        this.state.name.firstName === "Ifeanyi"
-          ? { firstName: "Mark", lastName: "Weber" }
-          : { firstName: "Ifeanyi", lastName: "Omeata" },
-    });
+    this.setState(
+      {
+        name:
+          this.state.name.firstName === "Ifeanyi"
+            ? { firstName: "Mark", lastName: "Weber" }
+            : { firstName: "Ifeanyi", lastName: "Omeata" },
+      },
+      () => {
+        console.log(this.state);
+      }
+    );
   }
 
   render() {
@@ -27,7 +32,7 @@ class App extends Component {
         <header className="App-header">
           <h1>Hello World!</h1>
           <h3>
-            Hi, my name is {this.state.name.firstName}{" "}
+            Hi, my name is {this.state.name.firstName}
             {this.state.name.lastName}. I work at {this.state.company}.
           </h3>
           <button onClick={this.handleChange}>Change Name</button>
