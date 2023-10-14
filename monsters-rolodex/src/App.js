b93893a0c31d983ae8cf0f1dadcd086c457e279f
omeatai <omeatai@gmail.com>
2023-10-13 18:58:26 -0600

    4. Creating Class Component State
    4. Creating Class Component State

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -2,11 +2,19 @@ import React, { Component } from "react";
 import "./App.css";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: "Ifeanyi",
+    };
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <h1>Hello World!</h1>
+          <h3>Hi, my name is {this.state.name}</h3>
         </header>
       </div>
     );
