07cc76f2c7ab4f7a34f9ca10c62db65b7c8d1edd
omeatai <omeatai@gmail.com>
2023-10-13 19:27:09 -0600

    11. Fetching Data with componentDidMount()
    11. Fetching Data with componentDidMount()

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -7,26 +7,28 @@ class App extends Component {
     this.state = {
       people: [],
     };
-
-    this.handleChange = this.handleChange.bind(this);
   }
 
-  async handleChange() {
-    const fetchResult = await fetch(
-      "https://jsonplaceholder.typicode.com/users"
-    )
-      .then((res) => {
-        if (res.ok) {
-          return res.json();
-        }
-        return Promise.reject(res);
-      })
-      .then((data) => Promise.resolve(data))
-      .catch((res) => console.error(res.status));
+  componentDidMount() {
+    const handleChange = async () => {
+      const fetchResult = await fetch(
+        "https://jsonplaceholder.typicode.com/users"
+      )
+        .then((res) => {
+          if (res.ok) {
+            return res.json();
+          }
+          return Promise.reject(res);
+        })
+        .then((data) => Promise.resolve(data))
+        .catch((res) => console.error(res.status));
 
-    this.setState({ people: fetchResult }, () => {
-      console.log("State: ", this.state);
-    });
+      this.setState({ people: fetchResult }, () => {
+        console.log("State: ", this.state);
+      });
+    };
+
+    handleChange();
   }
 
   render() {
@@ -34,7 +36,16 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <h2>List of People</h2>
-          {this.state.people.length > 0 ? (
+          {this.state.people ? null : (
+            <div>
+              <p
+                style={{ color: "red", fontSize: "42px", fontWeight: "bolder" }}
+              >
+                Error Fetching Data....
+              </p>
+            </div>
+          )}
+          {!this.state.people ? null : this.state.people.length > 0 ? (
             this.state.people.map((person) => {
               return (
                 <p key={person.id}>
@@ -45,7 +56,6 @@ class App extends Component {
           ) : (
             <div>
               <p>No Users Available</p>
-              <button onClick={this.handleChange}>Get Users</button>
             </div>
           )}
         </header>
