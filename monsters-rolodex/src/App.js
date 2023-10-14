43438eb4fcf83923cf55663ee7cdf14a80f94cd8
omeatai <omeatai@gmail.com>
2023-10-13 19:18:33 -0600

    10. Fetching Data with Asynchronous Function
    10. Fetching Data with Asynchronous Function

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -5,51 +5,49 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      name: { firstName: "Ifeanyi", lastName: "Omeata" },
-      company: "STZ",
-      monsters: [
-        { name: "Lindxa", id: "1" },
-        { name: "Frankr", id: "2" },
-        { name: "Jackoy", id: "3" },
-        { name: "Adrwei", id: "4" },
-      ],
+      people: [],
     };
 
     this.handleChange = this.handleChange.bind(this);
   }
 
-  handleChange() {
-    this.setState(
-      {
-        name:
-          this.state.name.firstName === "Ifeanyi"
-            ? { firstName: "Mark", lastName: "Weber" }
-            : { firstName: "Ifeanyi", lastName: "Omeata" },
-      },
-      () => {
-        console.log(this.state);
-      }
-    );
+  async handleChange() {
+    const fetchResult = await fetch(
+      "https://jsonplaceholder.typicode.com/users"
+    )
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        }
+        return Promise.reject(res);
+      })
+      .then((data) => Promise.resolve(data))
+      .catch((res) => console.error(res.status));
+
+    this.setState({ people: fetchResult }, () => {
+      console.log("State: ", this.state);
+    });
   }
 
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <h3>
-            Hi, my name is {this.state.name.firstName}
-            {this.state.name.lastName}. I work at {this.state.company}.
-          </h3>
-          <button onClick={this.handleChange}>Change Name</button>
-
-          <h2>List of Monsters</h2>
-          {this.state.monsters.map((monster) => {
-            return (
-              <p key={monster.id}>
-                {monster.id}. {monster.name}
-              </p>
-            );
-          })}
+          <h2>List of People</h2>
+          {this.state.people.length > 0 ? (
+            this.state.people.map((person) => {
+              return (
+                <p key={person.id}>
+                  {person.id}. {person.name}
+                </p>
+              );
+            })
+          ) : (
+            <div>
+              <p>No Users Available</p>
+              <button onClick={this.handleChange}>Get Users</button>
+            </div>
+          )}
         </header>
       </div>
     );
