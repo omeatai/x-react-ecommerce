5293898aabeffd423128df53d9cc5ca64df77428
omeatai <omeatai@gmail.com>
2023-10-14 22:28:33 -0600

    27. Creating Category Item Component
    27. Creating Category Item Component

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -1,4 +1,5 @@
 import "./App.scss";
+import CategoryItem from "./components/CategoryItem/CategoryItem";
 
 function App() {
   const categories = [
@@ -31,19 +32,8 @@ function App() {
 
   return (
     <div className="categories-container">
-      {categories.map(({ title, id, imageUrl }) => (
-        <div key={id} className="category-container">
-          <div
-            className="background-image"
-            style={{
-              backgroundImage: `url(${imageUrl})`,
-            }}
-          />
-          <div className="category-body-container">
-            <h2>{title}</h2>
-            <p>Shop Now</p>
-          </div>
-        </div>
+      {categories.map((category) => (
+        <CategoryItem key={category.id} category={category} />
       ))}
     </div>
   );
