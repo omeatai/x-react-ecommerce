c434d1d5f9324ba436f7c80e8d59a00ab3522915
omeatai <omeatai@gmail.com>
2023-10-17 12:00:37 -0600

    33. React Router - Setup Routes and Route
    33. React Router - Setup Routes and Route

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -1,8 +1,14 @@
 import "./App.scss";
+import { Routes, Route } from "react-router-dom";
 import Home from "./routes/Home/Home";
 
 const App = () => {
-  return <Home />;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/home" element={<Home />} />
+    </Routes>
+  );
 };
 
 export default App;
