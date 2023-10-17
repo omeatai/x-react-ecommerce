1b6f587b45b9beea2a3ed25d0fb611a2e7919bcb
omeatai <omeatai@gmail.com>
2023-10-17 12:03:44 -0600

    34. React Router - Create Shop Component
    34. React Router - Create Shop Component

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -1,12 +1,14 @@
 import "./App.scss";
 import { Routes, Route } from "react-router-dom";
 import Home from "./routes/Home/Home";
+import Shop from "./routes/Shop/Shop";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/home" element={<Home />} />
+      <Route path="/shop" element={<Shop />} />
     </Routes>
   );
 };
