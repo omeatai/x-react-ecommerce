01800abf7e5f3902476d97988a5877bc7f9fe5c9
omeatai <omeatai@gmail.com>
2023-10-17 12:35:33 -0600

    40. Firebase Authentication - Create Sign In Component
    40. Firebase Authentication - Create Sign In Component

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./routes/Home/Home";
 import Shop from "./routes/Shop/Shop";
 import Navigation from "./routes/Navigation/Navigation";
+import SignIn from "./routes/SignIn/SignIn";
 
 const App = () => {
   return (
@@ -10,6 +11,8 @@ const App = () => {
       <Route path="/" element={<Navigation />}>
         <Route path="home" element={<Home />} />
         <Route path="shop" element={<Shop />} />
+        <Route path="signin" element={<SignIn />} />
+        <Route path="/" element={<Home />} />
       </Route>
     </Routes>
   );
