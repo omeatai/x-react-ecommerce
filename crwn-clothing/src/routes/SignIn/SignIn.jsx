322c7a88e2604d353897fdd8592fb9e83d4f1702
omeatai <omeatai@gmail.com>
2023-10-17 13:36:58 -0600

    41. Firebase Authentication - Authenticating with Firebase
    41. Firebase Authentication - Authenticating with Firebase

--- crwn-clothing/src/routes/SignIn/SignIn.jsx
+++ crwn-clothing/src/routes/SignIn/SignIn.jsx
@@ -1,9 +1,15 @@
 import React from "react";
+import { signInWithGooglePopup } from "../../utils/firebase/firebase";
 
 const SignIn = () => {
+  const logGoogleuser = async () => {
+    const response = await signInWithGooglePopup();
+    console.log(response);
+  };
   return (
     <div>
       <h1>Sign In Page</h1>
+      <button onClick={logGoogleuser}>Sign in with Google Popup</button>
     </div>
   );
 };
