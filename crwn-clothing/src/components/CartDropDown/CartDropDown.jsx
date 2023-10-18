a2f5c4cc0672782145c753d73100d4bb1ed57a37
omeatai <omeatai@gmail.com>
2023-10-18 11:32:30 -0600

    60. React Context - Add To Cart Functionality
    60. React Context - Add To Cart Functionality

--- crwn-clothing/src/components/CartDropDown/CartDropDown.jsx
+++ crwn-clothing/src/components/CartDropDown/CartDropDown.jsx
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useContext } from "react";
+
 import Button from "../Button/Button";
+import CartItem from "../CartItem/CartItem";
+import { CartContext } from "../../contexts/cartContext";
+
 import "./CartDropDown.scss";
 
 const CartDropDown = () => {
+  const { cartItems } = useContext(CartContext);
+
   return (
     <div className="cart-dropdown-container">
-      <div className="cart-items"></div>
+      <div className="cart-items">
+        {cartItems.map((item) => (
+          <CartItem key={item.id} cartItem={item} />
+        ))}
+      </div>
       <Button>GO TO CHECKOUT</Button>
     </div>
   );
