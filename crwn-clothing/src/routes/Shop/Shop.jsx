d0d11cfca57429c3e366bcbd66f850c898d3cd00
omeatai <omeatai@gmail.com>
2023-10-18 10:43:38 -0600

    58. React Context - Create Product Card Component
    58. React Context - Create Product Card Component

--- crwn-clothing/src/routes/Shop/Shop.jsx
+++ crwn-clothing/src/routes/Shop/Shop.jsx
@@ -1,23 +1,16 @@
 import React, { useContext } from "react";
 import { ProductsContext } from "../../contexts/productsContext";
+import ProductCard from "../../components/ProductCard/ProductCard";
+import "./Shop.scss";
 
 const Shop = () => {
   const { products } = useContext(ProductsContext);
 
   return (
-    <div>
-      <h1>This is the Shop Page</h1>
-      <div>
-        {products.map(({ id, name, price, imageUrl }) => (
-          <div key={id}>
-            <h2>
-              {id}. {name}
-            </h2>
-            <h3>Price: {price} </h3>
-            <img src={imageUrl} alt="product" width={"200px"}></img>
-          </div>
-        ))}
-      </div>
+    <div className="shop-container">
+      {products.map((product) => (
+        <ProductCard key={product.id} product={product} />
+      ))}
     </div>
   );
 };
