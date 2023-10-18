4ab6973938d103863455df08d3c37a29ad40cf3d
omeatai <omeatai@gmail.com>
2023-10-18 10:34:29 -0600

    56. React Context - Create Shop Page
    56. React Context - Create Shop Page

--- crwn-clothing/src/routes/Shop/Shop.jsx
+++ crwn-clothing/src/routes/Shop/Shop.jsx
@@ -1,9 +1,21 @@
 import React from "react";
+import SHOP_DATA from "../../data/shop-data.json";
 
 const Shop = () => {
   return (
     <div>
       <h1>This is the Shop Page</h1>
+      <div>
+        {SHOP_DATA.map(({ id, name, price, imageUrl }) => (
+          <div key={id}>
+            <h2>
+              {id}. {name}
+            </h2>
+            <h3>Price: {price} </h3>
+            <img src={imageUrl} alt="product" width={"200px"}></img>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
