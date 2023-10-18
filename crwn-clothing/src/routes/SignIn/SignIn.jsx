0cfae7d040276f48d9e5efb1861486cf53fc50c9
omeatai <omeatai@gmail.com>
2023-10-17 23:24:52 -0600

    45. Firebase Authentication - Sign in with Redirect (Get User Google Credentials)
    45. Firebase Authentication - Sign in with Redirect (Get User Google Credentials)

--- crwn-clothing/src/routes/SignIn/SignIn.jsx
+++ crwn-clothing/src/routes/SignIn/SignIn.jsx
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { getRedirectResult } from "firebase/auth";
+
 import {
+  auth,
   signInWithGooglePopup,
   createUserDocumentFromAuth,
+  signInWithGoogleRedirect,
 } from "../../utils/firebase/firebase";
 
 const SignIn = () => {
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await getRedirectResult(auth);
+      console.log(response);
+    };
+    fetchData();
+  }, []);
+
   const logGoogleuser = async () => {
     const response = await signInWithGooglePopup();
     const userDocRef = await createUserDocumentFromAuth(response.user);
@@ -15,6 +27,9 @@ const SignIn = () => {
     <div>
       <h1>Sign In Page</h1>
       <button onClick={logGoogleuser}>Sign in with Google Popup</button>
+      <button onClick={signInWithGoogleRedirect}>
+        Sign in with Google Redirect
+      </button>
     </div>
   );
 };
--- crwn-clothing/src/utils/firebase/firebase.js
+++ crwn-clothing/src/utils/firebase/firebase.js
@@ -2,7 +2,7 @@
 import { initializeApp } from "firebase/app";
 import {
   getAuth,
-  //   signInWithRedirect,
+  signInWithRedirect,
   signInWithPopup,
   GoogleAuthProvider,
 } from "firebase/auth";
@@ -22,31 +22,28 @@ const firebaseConfig = {
 // Initialize Firebase
 const firebaseApp = initializeApp(firebaseConfig);
 
-// Google Provider Authentication
-const provider = new GoogleAuthProvider();
+const googleProvider = new GoogleAuthProvider();
 
-provider.setCustomParameters({
+googleProvider.setCustomParameters({
   prompt: "select_account",
 });
 
 export const auth = getAuth();
-export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+export const signInWithGooglePopup = () =>
+  signInWithPopup(auth, googleProvider);
+export const signInWithGoogleRedirect = () =>
+  signInWithRedirect(auth, googleProvider);
 
-//Check if the user record (document) exists in Firebase
 export const db = getFirestore();
 
 export const createUserDocumentFromAuth = async (userAuth) => {
-  // Get reference to user uid in the "users" table/collection
   const userDocRef = doc(db, "users", userAuth.uid);
-
-  // get the user record (document) in the "users" table/collection
   const userSnapshot = await getDoc(userDocRef);
 
-  // Checks if record (document) exists
-  //   console.log(userSnapshot.exists());
+  // Checks if document exists
+  console.log(userSnapshot.exists());
 
   if (!userSnapshot.exists()) {
-    //create a new user record/document in the "users" table/collection
     const { displayName, email } = userAuth;
     const createdAt = new Date();
     try {
@@ -58,10 +55,8 @@ export const createUserDocumentFromAuth = async (userAuth) => {
     } catch (error) {
       console.log("error creating the user", error.message);
     }
-    return userDocRef;
   } else {
-    console.log("User exists in database");
     console.log(userSnapshot);
-    return userDocRef;
   }
+  return userDocRef;
 };
