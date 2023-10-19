5bf06a55f0947aba8719ff6a2f931429fa0ef57b
omeatai <omeatai@gmail.com>
2023-10-19 14:50:06 -0600

    77. Styled Components - Using Styled Components for Form Input Component
    77. Styled Components - Using Styled Components for Form Input Component

--- crwn-clothing/src/components/FormInput/FormInput.jsx
+++ crwn-clothing/src/components/FormInput/FormInput.jsx
@@ -1,21 +1,22 @@
 import React from "react";
-import "./FormInput.scss";
+import { FormInputLabel, Input, Group } from "./FormInputStyle.js";
 
 const FormInput = ({ label, ...otherProps }) => {
   return (
-    <div className="group">
-      <input className="form-input" {...otherProps} />
+    <Group>
+      <Input {...otherProps} />
       {label && (
-        <label
-          className={`${
-            otherProps.value.length ? "shrink" : ""
-          } form-input-label`}
+        <FormInputLabel
+          shrink={otherProps.value.length}
           htmlFor={`${otherProps.id}`}
+          // className={`${
+          //   otherProps.value.length ? "shrink" : ""
+          // } form-input-label`}
         >
           {label}
-        </label>
+        </FormInputLabel>
       )}
-    </div>
+    </Group>
   );
 };
 
