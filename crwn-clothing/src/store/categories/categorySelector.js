191a268695528f85c869148630b533120be04d80
omeatai <omeatai@gmail.com>
2023-10-19 23:34:29 -0600

    91. Redux - Using Reselect Library (Memoized Selector)
    91. Redux - Using Reselect Library (Memoized Selector)

--- crwn-clothing/src/store/categories/categorySelector.js
+++ crwn-clothing/src/store/categories/categorySelector.js
@@ -1,9 +1,31 @@
-// export const selectCategoriesMap = (state) => state.categories.categoriesMap;
+import { createSelector } from "reselect";
 
-export const selectCategoriesMap = (state) => {
-  return state.categories.categories.reduce((acc, category) => {
-    const { title, items } = category;
-    acc[title.toLowerCase()] = items;
-    return acc;
-  }, {});
-};
+const selectCategoryReducer = (state) => state.categories;
+
+export const selectCategories = createSelector(
+  [selectCategoryReducer],
+  (categoriesSlice) => {
+    console.log("createSelector 1 fired...");
+    return categoriesSlice.categories;
+  }
+);
+
+export const selectCategoriesMap = createSelector(
+  [selectCategories],
+  (categories) => {
+    console.log("createSelector 2 fired...");
+    return categories.reduce((acc, category) => {
+      const { title, items } = category;
+      acc[title.toLowerCase()] = items;
+      return acc;
+    }, {});
+  }
+);
+
+// export const selectCategoriesMap = (state) => {
+//   return state.categories.categories.reduce((acc, category) => {
+//     const { title, items } = category;
+//     acc[title.toLowerCase()] = items;
+//     return acc;
+//   }, {});
+// };
