d15305e344d682ad1448d725a90e1982eb3594a6
omeatai <omeatai@gmail.com>
2023-10-19 23:49:43 -0600

    94. Redux - Using Redux Dev Tools
    94. Redux - Using Redux Dev Tools

--- crwn-clothing/src/store/store.js
+++ crwn-clothing/src/store/store.js
@@ -3,32 +3,29 @@ import {
   legacy_createStore as createStore,
   applyMiddleware,
 } from "redux";
-// import logger from "redux-logger";
+import logger from "redux-logger";
 import { rootReducer } from "./root-reducer";
 
 import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 
-const loggerMiddleware = (store) => (next) => (action) => {
-  if (!action.type) {
-    return next(action);
-  }
-  console.log("type: ", action.type);
-  console.log("payload: ", action.payload);
-  console.log("currentState: ", store.getState());
+const middleWares = [process.env.NODE_ENV !== "production" && logger].filter(
+  Boolean
+);
 
-  next(action);
-  console.log("next state: ", store.getState());
-};
+const composeEnhancer =
+  (process.env.NODE_ENV !== "production" &&
+    window &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+  compose;
 
-const middleWares = [loggerMiddleware];
-const composedEnhancers = compose(applyMiddleware(...middleWares));
+const composedEnhancers = composeEnhancer(applyMiddleware(...middleWares));
+// const composedEnhancers = compose(applyMiddleware(...middleWares));
 
 const persistConfig = { key: "root", storage, blacklist: ["user"] };
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 // createStore(reducer, [preloadedState], [enhancer])
-// export const store = createStore(rootReducer, undefined, composedEnhancers);
 export const store = createStore(
   persistedReducer,
   undefined,
