3a02e87ea7f0da10703c8314318b306697f8951b
omeatai <omeatai@gmail.com>
2023-11-06 09:54:16 -0700

    21. React Redux - Redux mapStateToProps
    21. React Redux - Redux mapStateToProps

--- react-redux-demo/src/App.js
+++ react-redux-demo/src/App.js
@@ -5,13 +5,15 @@ import "./App.css";
 // import CakeContainer from "./components/CakeContainer";
 // import HooksCakeContainer from "./components/HooksCakeContainer";
 // import IceCreamContainer from "./components/IceCreamContainer";
-import NewCakeContainer from "./components/NewCakeContainer";
+// import NewCakeContainer from "./components/NewCakeContainer";
+import ItemContainer from "./components/ItemContainer";
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
-        <NewCakeContainer />
+        <ItemContainer cake={"cake"} />
+        <ItemContainer />
       </div>
     </Provider>
   );
